02aced068e1480473d337c4f56c9adf7a2ea21fc
Yash Gupta <yguptangl@gmail.com>
2024-05-11 16:20:36 +0530

    adding all files

---
+++ business.js
@@ -0,0 +1,30 @@
+let main = document.getElementById("main-txt");
+
+let data ;
+let datalength = 0;
+async function getData(){
+    const res = await fetch("https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=d3c1020d2f634bbe905a2b53af580ec4");
+    const result  = await res.json();
+    data = result.articles;
+    datalength = data.length;
+    console.log(data);
+    renderUI();
+}
+
+getData();
+
+function renderUI() {
+  for (let i = 0; i < datalength; i++) {
+    let article = document.createElement("article");
+    article.innerHTML = `<article class="main">
+    <img src="${data[i].urlToImage}" alt="news">
+    <div class="art1">
+        <h2>${data[i].title}</h2>
+        <p>${data[i].description}</p>
+        <a href = "${data[i].url}" target = "_blank">Read More..</a>
+    </div>
+    
+    </article>`;
+main.appendChild(article);
+  }
+}
